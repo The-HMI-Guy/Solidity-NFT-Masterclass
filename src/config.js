2d4f967ee0b9d7225f95867720a3ca4ea0e4b103
Ray Ortiz <oray@bgsu.edu>
2022-05-17 18:48:28 -0400

    Adjustments to config to learn

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
+const namePrefix = "Cool Dragon";
+const description = "The HMI Guy's First NFT Attempt";
 const baseUri = "ipfs://NewUriToReplace";
 
 const solanaMetadata = {
@@ -24,7 +24,7 @@ const solanaMetadata = {
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 10,
     layersOrder: [
       { name: "Background" },
       { name: "Bottom lid" },
